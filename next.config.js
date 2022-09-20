41ba135dfafea2feba9a89a56472a85bcbcf05c2
js-code <jcsa031403@gmail.com>
2022-09-19 20:42:09 -0500

    adding dotenv

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
+require("dotenv").config();
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
