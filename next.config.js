d26f135597f5628a3986841ea1ef24c6ee0f0735
js-code <jcsa031403@gmail.com>
2022-09-19 20:55:04 -0500

    adding pre-commit hooks

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
-require("dotenv").config();
+require('dotenv').config()
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-};
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
